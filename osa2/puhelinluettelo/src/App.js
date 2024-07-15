f8733dfa6cf6a0a23db4b0b30a0ba386b3792c4b
Juha Sarkkinen <juha.sarkkinen@japsit.fi>
2024-07-15 13:15:57 +0300

    Tehty puhelinluettelo loppuun

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import puhelinluetteloService from './services/puhelinluettelo'
+import './index.css'
 
+const Notification = ({ message, status }) => {
+  if (message === null) {
+    return null
+  }
 
-const Person = ({ person }) => {
   return (
-    <p>{person.name} {person.number}</p>
+    <div className={status}>
+      {message}
+    </div>
+  )
+}
+
+const Person = ({ person, handleDelete }) => {
+  return (
+    <p>{person.name} {person.number} <DeleteButton id={person.id} handleDelete={handleDelete}></DeleteButton></p>
   )
 }
 
@@ -16,8 +29,7 @@ const SearchForm = ({ handleSearch, newSearch }) =>
     </form>
   );
 
-
-const AddPersonForm = ({handleNameChange, handleNumberChange, newName, newNumber, addName}) => (
+const AddPersonForm = ({ handleNameChange, handleNumberChange, newName, newNumber, addName }) => (
   <form>
   <div>
     name: <input onChange={handleNameChange} value={newName} />
@@ -31,46 +43,92 @@ const AddPersonForm = ({handleNameChange, handleNumberChange, newName, newNumber
 </form>
 )
 
-const ShowNumbers = ({persons}) => {
-  const personsToShow = persons;
+const DeleteButton = ({ id, handleDelete }) => (
+  <button onClick={() => handleDelete(id)}>Delete</button>
+)
+
+const ShowNumbers = ({ persons, handleDelete }) => {
   return (
     <div>
-  {personsToShow.map(person => 
-    <Person key={person.name}  person={person} /> )}
-  </div>
+      {persons.map(person => 
+        <Person key={person.id} person={person} handleDelete={handleDelete} /> )}
+    </div>
   );
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+  const [message, setMessage] = useState('')
+  const [status, setStatus] = useState('')
+  
+  useEffect(() => {
+    puhelinluetteloService.getAll().then(data => setPersons(data))
+  }, [])
+
+  useEffect(() => {
+    if (message) {
+      const messageTimer = setTimeout(() => {
+        setMessage(null)
+        setStatus('')
+      }, 3000)
+      return () => clearTimeout(messageTimer)
+    }
+  }, [message])
 
   const addName = (event) => {
     event.preventDefault()
 
-    const personExists = persons.some(person => person.name === newName)
+    const person = persons.find(person => person.name === newName)
 
-    if (personExists) {
-      alert(`${newName} is already added to phonebook`)
+    if (person) {
+      if (person && window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)) {
+        const updatedPerson = { ...person, number: newNumber}
+        puhelinluetteloService.update(person.id, updatedPerson).then( returnedData => {
+          setPersons(persons.map(p => p.id !== person.id ? p : returnedData))
+          setNewName('')
+          setNewNumber('')
+          setStatus('success')
+          setMessage(`Phone number changed for ${newName}`)
+        }).catch(error => {
+          setStatus('error')
+          setMessage(`'${person.name}' has already been deleted from server`)
+        })
+      }
     } else {
-      const person = {
+      const newPerson = {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(person))
-      setNewName('')
-      setNewNumber('')
+      puhelinluetteloService.create(newPerson).then(
+        returnedData => {
+          setPersons(persons.concat(returnedData))
+          setStatus('success')
+          setMessage(`Added ${newName}`)
+          setNewName('')
+          setNewNumber('')
+        }
+      )
     }
-    //console.log('button clicked', event.target)
   }
-  
+
+  const handleDelete = (id) => {
+    const person = persons.find(p => p.id === id)
+    if (person && window.confirm(`Delete ${person.name}?`)) {
+      puhelinluetteloService.del(id).then(() => {
+        setPersons(persons.filter(p => p.id !== id))
+        setStatus('success')
+        setMessage(`Deleted ${person.name}`)
+      }).catch(error => {
+        setStatus('error')
+        setMessage(`the note '${person.name}' was already deleted from server`)
+        setPersons(person.filter(n => n.id !== id))
+      })
+    }
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -86,22 +144,24 @@ const App = () => {
   const personsToShow = (newSearch === '')
     ? persons
     : persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
-  
 
   return (
     <div>
+      <Notification message={message} status={status} />
       <h2>Phonebook</h2>
       <SearchForm handleSearch={handleSearch} newSearch={newSearch} />
       <h2>Add a new</h2>  
-      <AddPersonForm handleNameChange={handleNameChange} newName={newName} handleNumberChange={handleNumberChange} newNumber={newNumber} addName={addName} />
+      <AddPersonForm 
+        handleNameChange={handleNameChange} 
+        newName={newName} 
+        handleNumberChange={handleNumberChange} 
+        newNumber={newNumber} 
+        addName={addName} 
+      />
       <h2>Numbers</h2>
-      <ShowNumbers persons={personsToShow} />
+      <ShowNumbers persons={personsToShow} handleDelete={handleDelete} />
     </div>
   )
-
-  
-
 }
-
 
 export default App
